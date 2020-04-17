5c4c9eed1ebb5b2e7b0b2df86106afec040d9577
cicciu <salvatore.cicciu@gmail.com>
2020-04-17 14:13:16 +0200

    local uid management

--- mvc/src/main/java/org/karnak/profile/action/UUID.java
+++ mvc/src/main/java/org/karnak/profile/action/UUID.java
@@ -3,11 +3,27 @@ package org.karnak.profile.action;
 import org.dcm4che6.data.DicomObject;
 import org.dcm4che6.data.VR;
 import org.dcm4che6.util.UIDUtils;
+
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 import org.dcm4che6.data.DicomElement;
 public class UUID implements Action {
+    private final Map<String, String> UIDMap = new HashMap<>();
+
     public void execute(DicomObject dcm, int tag, Iterator<DicomElement> iterator) {
-        dcm.setString(tag, VR.UI, UIDUtils.randomUID());
+
+        String uidValue = dcm.getString(tag).orElse(null);
+
+        if( UIDMap.containsKey(uidValue) ){
+            String uidDeidentValue = UIDMap.get(uidValue);
+            dcm.setString(tag, VR.UI, uidDeidentValue);
+        }else{
+            String uidDeidentValue =  UIDUtils.randomUID();
+            UIDMap.put(uidValue, uidDeidentValue);
+            dcm.setString(tag, VR.UI, uidDeidentValue);
+        }
+        
     }
 }
