83ca1fd3ef103247d1d8c12110dfd60871b3a65d
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2020-05-13 14:14:18 +0200

    Create dummy value hard coded

--- mvc/src/main/java/org/karnak/profile/option/dummyvalue/DefaultDummyValue.java
+++ mvc/src/main/java/org/karnak/profile/option/dummyvalue/DefaultDummyValue.java
@@ -3,44 +3,24 @@ package org.karnak.profile.option.dummyvalue;
 import org.dcm4che6.data.DicomObject;
 import org.dcm4che6.data.VR;
 import org.dcm4che6.util.UIDUtils;
-import org.karnak.data.AppConfig;
-import org.karnak.profile.HMAC;
-import org.karnak.profile.option.datemanager.ShiftDate;
-
 
 public class DefaultDummyValue {
-
-    private ShiftDate shiftDate = new ShiftDate();
-    private final int maxDays = 365;
-    private final int maxSeconds = 24*60*60;
-    private final int nbBytesFormatDA = 8;
-
-    private HMAC hmac;
-    {
-        hmac = AppConfig.getInstance().getHmac();
-    }
-
     public DefaultDummyValue() {
     }
 
     public String execute(VR vr, DicomObject dcm, int tag, String patientID) {
         String stringValue = dcm.getString(tag).orElse(null);
         if (stringValue != null) {
-            /*
-            * SV, UV -> Not present in class dcm4che.VR
-            * */
             String dummyValue = switch (vr) {
                 case AE, CS, LO, LT, PN, SH, ST, UN, UT, UC, UR -> unknownValue();
                 case DS, FL, FD, IS, SL, SS, UL, US -> zeroValue();
-                case AS -> AS(stringValue, patientID);
-                case DA -> DA(stringValue, patientID);
-                case DT -> DT(stringValue, patientID);
-                case TM -> TM(stringValue, patientID);
+                case AS -> AS();
+                case DA -> DA();
+                case DT -> DT();
+                case TM -> TM();
                 case UI -> UI();
                 default -> notImplemented();
             };
-            System.out.println(stringValue);
-            System.out.println(dummyValue);
             return dummyValue;
         }
         return null;
@@ -58,34 +38,20 @@ public class DefaultDummyValue {
         return "0";
     }
 
-    private String AS(String age, String patientID) {
-        double shiftDays = this.hmac.scaleHash(patientID, 0, this.maxDays);
-        return this.shiftDate.ASbyDays(age, (int) shiftDays);
+    private String AS() {
+        return "045Y";
     }
 
-    private String TM(String time, String patientID) {
-        double shiftSeconds = this.hmac.scaleHash(patientID, 0, this.maxSeconds);
-        return this.shiftDate.TMbySeconds(time, (int) shiftSeconds);
+    private String TM() {
+        return "111111";
     }
 
-    private String DA(String date, String patientID) {
-        double shiftDays = this.hmac.scaleHash(patientID, 0, this.maxDays);
-        return this.shiftDate.DAbyDays(date, (int) shiftDays);
+    private String DA() {
+        return "19991111";
     }
 
-    private String DT(String datetime, String patientID) {
-        String dummyDate = null;
-        int datetimeLength = datetime.length();
-        if (datetimeLength > this.nbBytesFormatDA) {
-            String date = datetime.substring(0, this.nbBytesFormatDA);
-            dummyDate = DA(date, patientID);
-            String dummyTime = null;
-            String time = datetime.substring(8, datetimeLength);
-            dummyTime = TM(time, patientID);
-            return dummyDate.concat(dummyTime);
-        }
-        dummyDate = DA(datetime, patientID);
-        return dummyDate;
+    private String DT() {
+        return "19991111111111";
     }
 
     private String UI() {
