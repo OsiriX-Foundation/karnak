141fe9fe38de815cb268e3c09faa4f644e6c1edd
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2020-05-13 15:19:02 +0200

    Remove useless import

--- mvc/src/main/java/org/karnak/profile/action/UUID.java
+++ mvc/src/main/java/org/karnak/profile/action/UUID.java
@@ -2,15 +2,11 @@ package org.karnak.profile.action;
 
 import org.dcm4che6.data.DicomObject;
 import org.dcm4che6.data.VR;
-import org.dcm4che6.util.UIDUtils;
 
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map;
 
 import org.dcm4che6.data.DicomElement;
 import org.karnak.data.AppConfig;
-import org.karnak.data.profile.ProfilePersistence;
 import org.karnak.profile.HMAC;
 
 public class UUID implements Action {
