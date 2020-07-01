ba10af9245ff62337a29c2df56f3fd53eec900a4
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2020-07-01 15:11:25 +0200

    Write SOPS defined in the DICOM Standard in database

--- data/src/main/java/org/karnak/data/gateway/SOPClassUIDPersistence.java
+++ data/src/main/java/org/karnak/data/gateway/SOPClassUIDPersistence.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SOPClassUIDPersistence extends JpaRepository<SOPClassUID, Long> {
     SOPClassUID getSOPClassUIDByName(String name);
+
+    Boolean existsByCiodAndUidAndName(String ciod, String uid, String name);
 }
--- mvc/src/main/java/org/karnak/standard/StandardDICOM.java
+++ mvc/src/main/java/org/karnak/standard/StandardDICOM.java
@@ -1,12 +1,14 @@
 package org.karnak.standard;
 
+import org.karnak.data.gateway.SOPClassUID;
+import org.karnak.data.gateway.SOPClassUIDPersistence;
 import org.karnak.standard.dicominnolitics.*;
-import org.w3c.dom.Attr;
+import org.karnak.ui.gateway.GatewayConfiguration;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
 public class StandardDICOM {
     private static StandardSOPS standardSOPS;
@@ -14,12 +16,15 @@ public class StandardDICOM {
     private static StandardCIODtoModules standardCIODtoModules;
     private static StandardModuleToAttributes standardModuleToAttributes;
 
+    private SOPClassUIDPersistence sopClassUIDPersistence = GatewayConfiguration.getInstance().getSopClassUIDPersistence();
+
     public StandardDICOM() {
         standardSOPS = new StandardSOPS();
         standardCIODS = new StandardCIODS();
         standardCIODtoModules = new StandardCIODtoModules();
         standardModuleToAttributes = new StandardModuleToAttributes();
 
+        insertSOPSClassInDB(standardSOPS.getSOPS());
         SOPS sops = new SOPS(standardSOPS.getSOPS(), standardCIODS.getCIODS(), standardCIODtoModules.getCIODToModules());
         Attributes attributes = new Attributes(standardModuleToAttributes.getModuleToAttributes());
         ArrayList<String> allUIDs = sops.getAllUIDs();
@@ -32,5 +37,18 @@ public class StandardDICOM {
         boolean present = sops.moduleIsPresent("1.2.840.10008.5.1.4.1.1.2", "patient");
 
         List<Attribute> moduleAttributes = attributes.getAttributesByModule("patient");
+    }
+
+    private void insertSOPSClassInDB(jsonSOP[] sops) {
+        Set<SOPClassUID> sopClassUIDSet = new HashSet<>();
+        for (jsonSOP sop : sops) {
+            final String ciod = sop.getCiod();
+            final String uid = sop.getId();
+            final String name = sop.getName();
+            if (sopClassUIDPersistence.existsByCiodAndUidAndName(ciod, uid, name).equals(Boolean.FALSE)) {
+                sopClassUIDSet.add(new SOPClassUID(ciod, uid, name));
+            }
+        }
+        sopClassUIDPersistence.saveAll(sopClassUIDSet);
     }
 }
