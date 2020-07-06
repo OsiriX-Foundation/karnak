f2b5c60e43c70eeab9038e2c4159e92c413a88f1
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2020-07-06 16:37:55 +0200

    Use tagPattern and catch some errors from the yaml read.

--- mvc/src/main/java/org/karnak/profileschain/profiles/ActionTags.java
+++ mvc/src/main/java/org/karnak/profileschain/profiles/ActionTags.java
@@ -1,28 +1,42 @@
 package org.karnak.profileschain.profiles;
 
 import org.dcm4che6.data.DicomElement;
-import org.dcm4che6.util.TagUtils;
 import org.karnak.profileschain.action.Action;
+import org.karnak.profileschain.utils.TagActionMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class ActionTags extends AbstractProfileItem {
-    public ActionTags(String name, String codeName, String action, List<String> tags) {
+    private final Logger LOGGER = LoggerFactory.getLogger(ActionTags.class);
+    private TagActionMap tagsAction;
+
+    public ActionTags(String name, String codeName, String action, List<String> tags) throws Exception {
         super(name, codeName, action, tags);
+        tagsAction = new TagActionMap();
         setActionHashMap();
     }
 
-    private void setActionHashMap() {
+    private void setActionHashMap() throws Exception {
         Action action = stringToAction();
+
+        if (stringToAction() == null) {
+            throw new Exception("Cannot build the profile " + codeName + ": Unknown Action");
+        }
+
+        if (tags == null || tags.size() == 0) {
+            throw new Exception("Cannot build the profile " + codeName + ": No tags defined");
+        }
+
         for (String tag: tags) {
-            int tagInt = TagUtils.intFromHexString(tag.replaceAll("[(),]", ""));
-            this.put(tagInt, action);
+            tagsAction.put(tag, action);
         }
     }
 
     @Override
     public Action getAction(DicomElement dcmElem) {
-        return this.tagMap.get(dcmElem.tag());
+        return tagsAction.get(dcmElem.tag());
     }
 
     public Action stringToAction() {
@@ -31,7 +45,6 @@ public class ActionTags extends AbstractProfileItem {
             case "REMOVE" -> Action.REMOVE;
             case "KEEP" -> Action.KEEP;
             case "REPLACE_UID" -> Action.UID;
-            case "REPLACE" -> Action.REPLACE;
             default -> null;
         };
     }
