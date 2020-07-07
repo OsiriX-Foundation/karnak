8118e6872f9d64ad49b858cc4be5c77c2f923b62
cicciu <salvatore.cicciu@gmail.com>
2020-07-07 11:44:15 +0200

    Added filterBySOPClasses checkbox control in GatewayConfig

--- mvc/src/main/java/org/karnak/service/GatewayConfig.java
+++ mvc/src/main/java/org/karnak/service/GatewayConfig.java
@@ -247,9 +247,12 @@ public class GatewayConfig {
                                     Destination dstNode) {
         try {
             List<AttributeEditor> editors = new ArrayList<>();
-            editors.add(new FilterEditor(dstNode.getSOPClassUIDFilters()));
-            boolean desidentificationEnable = dstNode.getDesidentification();
-            if(desidentificationEnable == true){ //TODO add an option in destination model
+            final boolean filterBySOPClassesEnable = dstNode.getFilterBySOPClasses();
+            if(filterBySOPClassesEnable) {
+                editors.add(new FilterEditor(dstNode.getSOPClassUIDFilters()));
+            }
+            final boolean desidentificationEnable = dstNode.getDesidentification();
+            if(desidentificationEnable){ //TODO add an option in destination model
                 editors.add(new DeidentifyEditor());
             }
 
