83e80e2468704aaf4e56b605115402666f0de9ea
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2020-07-15 16:52:34 +0200

    Use comboBox instead of select

--- mvc/src/main/java/org/karnak/ui/gateway/ProfileDropDown.java
+++ mvc/src/main/java/org/karnak/ui/gateway/ProfileDropDown.java
@@ -1,16 +1,12 @@
 package org.karnak.ui.gateway;
 
-import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.combobox.ComboBox;
 import org.karnak.data.profile.ProfilePipe;
 import org.karnak.ui.profile.ProfilePipeService;
 import org.karnak.ui.profile.ProfilePipeServiceImpl;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-public class ProfileDropDown extends Select<ProfilePipe> {
+public class ProfileDropDown extends ComboBox<ProfilePipe> {
     private ProfilePipeService profilePipeService;
-    private List<ProfilePipe> profilePipes;
 
     public ProfileDropDown() {
         profilePipeService = new ProfilePipeServiceImpl();
@@ -18,10 +14,7 @@ public class ProfileDropDown extends Select<ProfilePipe> {
     }
 
     public void updateList() {
-        profilePipes = profilePipeService.getAllProfiles();
         setItems(profilePipeService.getAllProfiles());
         setItemLabelGenerator(ProfilePipe::getName);
-        setValue(getValue());
     }
-
 }
