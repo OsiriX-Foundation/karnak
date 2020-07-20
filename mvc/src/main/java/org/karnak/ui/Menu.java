6fb0ca18069be9e89648632d22de6a5b930c39fa
cicciu <salvatore.cicciu@gmail.com>
2020-07-20 17:15:00 +0200

    Add radiobox for choose dark theme or light theme

--- mvc/src/main/java/org/karnak/ui/Menu.java
+++ mvc/src/main/java/org/karnak/ui/Menu.java
@@ -1,25 +1,24 @@
 package org.karnak.ui;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.icon.IronIcon;
+import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
+import com.vaadin.flow.dom.ThemeList;
+import com.vaadin.flow.theme.lumo.Lumo;
 import org.karnak.ui.authentication.AccessControlFactory;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.RouterLink;
-import com.vaadin.flow.server.VaadinServletService;
-import com.vaadin.flow.server.VaadinSession;
 
 @NpmPackage(value = "@polymer/iron-icons", version = "3.0.1")
 @JsModule("@polymer/iron-icons/iron-icons.js")
@@ -28,6 +27,7 @@ public class Menu extends FlexLayout {
     private static final String SHOW_TABS = "show-tabs";
 
     private Tabs tabs;
+    private RadioButtonGroup<String> radioGroup;
 
     public Menu() {
         setClassName("menu-bar");
@@ -50,6 +50,23 @@ public class Menu extends FlexLayout {
         tabs.setOrientation(Tabs.Orientation.VERTICAL);
         setFlexGrow(1, tabs);
         add(tabs);
+
+        // Theme color
+        radioGroup = new RadioButtonGroup<>();
+        radioGroup.setLabel("Thème");
+        radioGroup.setItems("Light", "Dark");
+        radioGroup.setValue("Light");
+        radioGroup.addValueChangeListener(radioButtonGroupStringComponentValueChangeEvent -> {
+            ThemeList themeList = UI.getCurrent().getElement().getThemeList(); //
+
+            if (themeList.contains(Lumo.DARK)) { //
+                themeList.remove(Lumo.DARK);
+            } else {
+                themeList.add(Lumo.DARK);
+            }
+        });
+
+        add(radioGroup);
 
         // logout menu item
         Button logoutButton = new Button("Logout", VaadinIcon.SIGN_OUT.create());
