5d1c37b7211e4de760a2d701d8ffc81d03ad6a34
Nicolas Roduit <nicolas.roduit@gmail.com>
2020-08-07 15:31:29 +0200

    Update lib and set help references

--- mvc/src/main/java/org/karnak/ui/help/HelpView.java
+++ mvc/src/main/java/org/karnak/ui/help/HelpView.java
@@ -2,26 +2,34 @@ package org.karnak.ui.help;
 
 
 import com.vaadin.flow.component.Tag;
-import com.vaadin.flow.component.dependency.JsModule;
-import com.vaadin.flow.component.dependency.NpmPackage;
-import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.templatemodel.TemplateModel;
 import org.karnak.ui.MainLayout;
 
 @Route(value = "help", layout = MainLayout.class)
 @PageTitle("KARNAK - Help")
 @Tag("help-view")
-@NpmPackage(value = "@polymer/paper-input", version = "3.0.2")
-@JsModule("./js/help.js")
-public class HelpView extends PolymerTemplate<TemplateModel> {
+public class HelpView extends VerticalLayout {
     public static final String VIEW_NAME = "Help";
 
 
     public HelpView() {
-        setId("template");
-        getElement().getStyle().set("overflow-y", "auto").set("padding-left", "15px");
+        setSizeFull();
+        H1 heading = new H1( "Help" );
 
+        Anchor generalDoc = new Anchor( "https://github.com/OsiriX-Foundation/karnak-docker/blob/master/README.md" , "General documentation");
+        generalDoc.setTarget( "_blank" );
+
+        Anchor installation = new Anchor( "https://github.com/OsiriX-Foundation/karnak-docker/blob/master/README.md" , "Installation and configuration with Docker");
+        installation.setTarget( "_blank" );
+
+        Anchor profile = new Anchor( "https://github.com/OsiriX-Foundation/karnak-docker/tree/master/profileExample" , "Build your own profile for de-identification or for tag morphing");
+        profile.setTarget( "_blank" );
+        VerticalLayout layout = new VerticalLayout();
+        layout.add(heading, generalDoc,  installation, profile );
+        this.add( layout);
     }
 }
