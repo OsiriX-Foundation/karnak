231ce5e7c0268da61b5f82099d5b97c9e1acc5fb
cicciu <salvatore.cicciu@gmail.com>
2020-08-10 14:54:21 +0200

    Adding conditionInterpreter tests

--- mvc/src/test/java/org/karnak/profilepipe/utils/MyDCMElemTest.java
+++ mvc/src/test/java/org/karnak/profilepipe/utils/MyDCMElemTest.java
@@ -1,11 +1,8 @@
 package org.karnak.profilepipe.utils;
 
-import org.dcm4che6.data.Tag;
 import org.dcm4che6.data.VR;
+import org.dcm4che6.util.TagUtils;
 import org.junit.jupiter.api.Test;
-import org.springframework.expression.EvaluationContext;
-import org.springframework.expression.Expression;
-import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,6 +10,24 @@ class MyDCMElemTest {
 
     @Test
     void conditionInterpreter() {
+        final MyDCMElem myDCMElem = new MyDCMElem(TagUtils.intFromHexString("00100010"), VR.PN, "CARDIX");
+        assertEquals("tag == 1048592 and stringValue == 'CARDIX'", myDCMElem.conditionInterpreter("tag == (0010,0010) and stringValue == 'CARDIX'"));
+        assertEquals("tag == 1048592", myDCMElem.conditionInterpreter("tag == (0010,00xx)"));
+        assertEquals("tag == 1048592 and stringValue == 'CARDIX'", myDCMElem.conditionInterpreter("tag == 0010,00x0) and stringValue == 'CARDIX'"));
+        assertEquals("tag == 1048592", myDCMElem.conditionInterpreter("tag == (00x0,0010"));
+        assertEquals("tag == 1048592", myDCMElem.conditionInterpreter("tag == 001x00x0"));
+
+        final MyDCMElem myDCMElem2 = new MyDCMElem(TagUtils.intFromHexString("00100020"), VR.PN, "CARDIX");
+        assertEquals("tag == 1048608 and tag == #TAG.PatientName", myDCMElem2.conditionInterpreter("tag == (00x0,0020) and tag == #TAG.PatientName"));
+        assertEquals("tag == 1048608", myDCMElem2.conditionInterpreter("tag == 001xxx20"));
+        assertEquals("tag == 1048608", myDCMElem2.conditionInterpreter("tag == 0010002x"));
+        assertEquals("tag == 1048608", myDCMElem2.conditionInterpreter("tag == x010002x"));
+
+        final MyDCMElem myDCMElem3 = new MyDCMElem(TagUtils.intFromHexString("02100220"), VR.PN, "CADRIX");
+        assertEquals("tag == 34603552", myDCMElem3.conditionInterpreter("tag == 02100220)"));
+        assertEquals("tag == 34603552", myDCMElem3.conditionInterpreter("tag == (02100220"));
+        assertEquals("tag == 34603552", myDCMElem3.conditionInterpreter("tag == 0210,0220"));
+        assertEquals("tag == 34603552", myDCMElem3.conditionInterpreter("tag == 0210,0220)"));
     }
 
     @Test
