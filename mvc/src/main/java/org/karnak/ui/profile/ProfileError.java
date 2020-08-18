fa7cb09d2899c74fb8cbb24fec4fae9223b8907e
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2020-08-18 15:03:34 +0200

    Add validateProfile function

---
+++ mvc/src/main/java/org/karnak/ui/profile/ProfileError.java
@@ -0,0 +1,34 @@
+package org.karnak.ui.profile;
+
+import org.karnak.data.profile.ProfileElement;
+
+public class ProfileError {
+    private ProfileElement profileElement;
+    private String error;
+
+    public ProfileError(ProfileElement profileElement) {
+        this.profileElement = profileElement;
+        this.error = null;
+    }
+
+    public ProfileError(ProfileElement profileElement, String error) {
+        this.profileElement = profileElement;
+        this.error = error;
+    }
+
+    public ProfileElement getProfileElement() {
+        return profileElement;
+    }
+
+    public void setProfileElement(ProfileElement profileElement) {
+        this.profileElement = profileElement;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
