a703756662c76b670b788ee86a2eac20e91edcad
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2020-09-29 15:22:14 +0200

    Remove in the database KheopsAlbums

--- data/src/main/java/org/karnak/data/gateway/KheopsAlbumsPersistence.java
+++ data/src/main/java/org/karnak/data/gateway/KheopsAlbumsPersistence.java
@@ -3,6 +3,9 @@ package org.karnak.data.gateway;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface KheopsAlbumsPersistence extends JpaRepository<KheopsAlbums, Long> {
+    List<KheopsAlbums> findAllByDestination(Destination destination);
 }
