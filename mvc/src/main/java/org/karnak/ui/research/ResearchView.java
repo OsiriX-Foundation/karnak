9abaed09419f83164db9da6f62856cd6f7dc71be
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2020-10-09 14:13:15 +0200

    Create grid to list the project

--- mvc/src/main/java/org/karnak/ui/research/ResearchView.java
+++ mvc/src/main/java/org/karnak/ui/research/ResearchView.java
@@ -2,6 +2,7 @@ package org.karnak.ui.research;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.karnak.data.gateway.KheopsAlbums;
@@ -13,12 +14,16 @@ import org.karnak.ui.MainLayout;
 public class ResearchView extends VerticalLayout {
     public static final String VIEW_NAME = "Research";
     private NewResearchForm newResearchForm;
+    private GridResearch gridResearch;
     private Binder<Research> newResearchBinder;
+    private ListDataProvider<Research> dataProviderResearch;
 
     public ResearchView() {
         newResearchForm = new NewResearchForm();
+        gridResearch = new GridResearch();
+        dataProviderResearch = (ListDataProvider<Research>) gridResearch.getDataProvider();
         newResearchBinder = newResearchForm.getBinder();
-        add(newResearchForm);
+        add(newResearchForm, gridResearch);
         setEventButtonAdd();
     }
 
@@ -26,10 +31,8 @@ public class ResearchView extends VerticalLayout {
         newResearchForm.getButtonAdd().addClickListener(event -> {
             Research newResearch = new Research();
             if (newResearchBinder.writeBeanIfValid(newResearch)) {
-                /*
-                dataProviderSwitchingAlbums.getItems().add(newKheopsAlbums);
-                dataProviderSwitchingAlbums.refreshAll();
-                */
+                dataProviderResearch.getItems().add(newResearch);
+                dataProviderResearch.refreshAll();
                 newResearchForm.clear();
             }
         });
