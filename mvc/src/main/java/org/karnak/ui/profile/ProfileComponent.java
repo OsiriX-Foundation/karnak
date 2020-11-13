ffd777a820165a6f35f2172ecf0bf0e2b2657ff0
cicciu <salvatore.cicciu@gmail.com>
2020-11-13 14:59:27 +0100

    added exporting profile with downloading button

--- mvc/src/main/java/org/karnak/ui/profile/ProfileComponent.java
+++ mvc/src/main/java/org/karnak/ui/profile/ProfileComponent.java
@@ -1,13 +1,30 @@
 package org.karnak.ui.profile;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.server.StreamResource;
 import org.karnak.data.profile.Profile;
+import org.karnak.data.profile.ProfileElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayInputStream;
+import java.util.Comparator;
 
 public class ProfileComponent extends VerticalLayout {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProfileComponent.class);
+
     private Profile profile;
     private ProfilePipeService profilePipeService;
     private ProfileNameGrid profileNameGrid;
+    private Anchor download;
 
     ProfileComponent(ProfilePipeService profilePipeService, ProfileNameGrid profileNameGrid) {
         setSizeFull();
@@ -42,7 +59,12 @@ public class ProfileComponent extends VerticalLayout {
             updatedProfilePipes();
         });
 
-        add(title, name, version, minVersion, defaultIssuerOfPatientID);
+        final StreamResource profileStreamResourse = createStreamResource(profile);
+        download = new Anchor(profileStreamResourse, "");
+        download.getElement().setAttribute("download", true);
+        download.add(new Button("Download profile",new Icon(VaadinIcon.DOWNLOAD_ALT)));
+
+        add(title, name, version, minVersion, defaultIssuerOfPatientID, download);
     }
 
     private void updatedProfilePipes() {
@@ -65,5 +87,20 @@ public class ProfileComponent extends VerticalLayout {
             this.profile = profile;
             setProfile();
         }
+    }
+
+    public static StreamResource createStreamResource(Profile profile) {
+        try{
+            profile.getProfileElements().sort(Comparator.comparingInt(ProfileElement::getPosition));
+            //https://stackoverflow.com/questions/61506368/formatting-yaml-with-jackson
+            ObjectMapper mapper = new ObjectMapper(new YAMLFactory().disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER));
+
+            String strYaml = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(profile);
+            StreamResource streamResource = new StreamResource(String.format("%s.yml",profile.getName()).replace(" ", "-"), () -> new ByteArrayInputStream(strYaml.getBytes()));
+            return streamResource;
+        } catch (final Exception e) {
+            LOGGER.error("Cannot create the StreamResource for downloading the yaml profile", e);
+        }
+        return null;
     }
 }
