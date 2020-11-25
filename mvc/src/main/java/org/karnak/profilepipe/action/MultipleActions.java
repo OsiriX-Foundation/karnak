33feda4bb48f81cdc96abaadfe84fdfa0424908d
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2020-11-25 17:11:20 +0100

    Manage multiple attributes (duplicate tag in the standard)

--- mvc/src/main/java/org/karnak/profilepipe/action/MultipleActions.java
+++ mvc/src/main/java/org/karnak/profilepipe/action/MultipleActions.java
@@ -3,16 +3,17 @@ package org.karnak.profilepipe.action;
 import org.dcm4che6.data.DicomElement;
 import org.dcm4che6.data.DicomObject;
 import org.dcm4che6.data.Tag;
-import org.dcm4che6.util.TagUtils;
 import org.karnak.data.AppConfig;
 import org.karnak.profilepipe.utils.HMAC;
 import org.karnak.profilepipe.utils.MetadataDICOMObject;
 import org.karnak.standard.Attribute;
+import org.karnak.standard.Module;
 import org.karnak.standard.StandardDICOM;
 import org.karnak.standard.exceptions.StandardDICOMException;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MultipleActions extends AbstractAction {
     final StandardDICOM standardDICOM;
@@ -43,15 +44,37 @@ public class MultipleActions extends AbstractAction {
                 ActionItem actionItem = chooseAction(sopUID, currentType);
                 actionItem.execute(dcm, tag, iterator, hmac);
             } else if (attributes.size() > 1) {
-                // TODO: Choose the action by module ...
-                ActionItem defaultDummy = new DefaultDummy(symbol);
-                defaultDummy.execute(dcm, tag, iterator, hmac);
+                ActionItem action = multipleAttributes(sopUID, attributes);
+                action.execute(dcm, tag, iterator, hmac);
             }
         } catch (StandardDICOMException standardDICOMException) {
             LOGGER.error("Could not execute an action with a unknown SOP", standardDICOMException);
         }
         // TODO: throw exception
         // Throw exception Tag NOT FOUND IN THE DICOM Standard ?
+    }
+
+    private ActionItem multipleAttributes(String sopUID, List<Attribute> attributes) {
+        List<Module> modules = attributes.stream()
+                .map(attribute -> standardDICOM.getModuleByModuleID(sopUID, attribute.getModuleId()).orElse(null))
+                .collect(Collectors.toList());
+        List<Module> modulesMandatory = modules.stream()
+                .filter(module -> Module.moduleIsMandatory(module))
+                .collect(Collectors.toList());
+
+        if (modulesMandatory.size() == 0) {
+            return actionRemove;
+        }
+
+        if (modulesMandatory.size() == 1) {
+            String currentType = attributes.stream()
+                    .filter(attr -> attr.getModuleId().equals(modulesMandatory.get(0).getId()))
+                    .findFirst().orElse(null).getType();
+            return chooseAction(sopUID, currentType);
+        }
+
+        String currentType = Attribute.getStrictedType(attributes);
+        return chooseAction(sopUID, currentType);
     }
 
     private ActionItem chooseAction(String sopUID, String currentType) {
