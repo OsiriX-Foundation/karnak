72880ffc84b2fd4389f83a4ac60191f4ebb3ece6
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2020-11-25 16:21:31 +0100

    Use a RuntimeException instead of Exception

--- mvc/src/main/java/org/karnak/standard/exceptions/StandardDICOMException.java
+++ mvc/src/main/java/org/karnak/standard/exceptions/StandardDICOMException.java
@@ -1,6 +1,6 @@
 package org.karnak.standard.exceptions;
 
-public abstract class StandardDICOMException extends Exception {
+public abstract class StandardDICOMException extends RuntimeException {
     public StandardDICOMException(String message) {
         super(message);
     }
