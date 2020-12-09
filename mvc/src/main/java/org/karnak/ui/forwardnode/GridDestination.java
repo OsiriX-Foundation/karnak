dc511bf8e3dcb70969a251f4d7c45209a8f20d21
cicciu <salvatore.cicciu@gmail.com>
2020-12-09 10:40:12 +0100

    color circle for displaying if the destination is enabled or disabled.

--- mvc/src/main/java/org/karnak/ui/forwardnode/GridDestination.java
+++ mvc/src/main/java/org/karnak/ui/forwardnode/GridDestination.java
@@ -1,6 +1,7 @@
 package org.karnak.ui.forwardnode;
 
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Span;
 import org.karnak.data.gateway.Destination;
 
 public class GridDestination extends Grid<Destination> {
@@ -10,6 +11,19 @@ public class GridDestination extends Grid<Destination> {
         addColumn(Destination::getDescription).setHeader("Description").setFlexGrow(20).setSortable(true);
 
         addColumn(Destination::getType).setHeader("Type").setFlexGrow(20).setSortable(true);
+
+        addComponentColumn(destination -> {
+            Span spanDot = new Span();
+            spanDot.getStyle().set("height", "25px");
+            spanDot.getStyle().set("width", "25px");
+            spanDot.getStyle().set("border-radius", "50%");
+            spanDot.getStyle().set("display", "inline-block");
+            if (destination.getState()) {
+                spanDot.getStyle().set("background-color", "#5FC04C");
+            } else {
+                spanDot.getStyle().set("background-color", "#FC4848");         }
+            return spanDot;
+        }).setHeader("Enabled").setFlexGrow(20).setSortable(true);
     }
 
     public Destination getSelectedRow() {
