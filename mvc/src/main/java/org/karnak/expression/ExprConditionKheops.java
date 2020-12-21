aead8c1f1b2a29d0088ca794fa7dcca4c540ff1a
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2020-12-21 16:08:41 +0100

    Clean code

--- mvc/src/main/java/org/karnak/expression/ExprConditionKheops.java
+++ mvc/src/main/java/org/karnak/expression/ExprConditionKheops.java
@@ -1,15 +1,7 @@
 package org.karnak.expression;
 
 import org.dcm4che6.data.DicomObject;
-import org.dcm4che6.data.Tag;
 import org.dcm4che6.util.TagUtils;
-import org.weasis.core.util.StringUtil;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class ExprConditionKheops implements ExpressionItem{
     private DicomObject dcm;
@@ -25,7 +17,7 @@ public class ExprConditionKheops implements ExpressionItem{
 
     public boolean tagValueIsPresent(int tag, String value) {
         String dcmValue = dcm.getString(tag).orElse(null);
-        return dcmValue != null ? dcmValue.equals(value) : false;
+        return dcmValue != null && dcmValue.equals(value);
     }
 
     public boolean tagValueContains(String tag, String value) {
@@ -35,7 +27,7 @@ public class ExprConditionKheops implements ExpressionItem{
 
     public boolean tagValueContains(int tag, String value) {
         String dcmValue = dcm.getString(tag).orElse(null);
-        return dcmValue != null ? dcmValue.contains(value) : false;
+        return dcmValue != null && dcmValue.contains(value);
     }
 
     public boolean tagValueBeginsWith(String tag, String value) {
@@ -45,7 +37,7 @@ public class ExprConditionKheops implements ExpressionItem{
 
     public boolean tagValueBeginsWith(int tag, String value) {
         String dcmValue = dcm.getString(tag).orElse(null);
-        return dcmValue != null ? dcmValue.startsWith(value) : false;
+        return dcmValue != null && dcmValue.startsWith(value);
     }
 
     public boolean tagValueEndsWith(String tag, String value) {
@@ -55,10 +47,10 @@ public class ExprConditionKheops implements ExpressionItem{
 
     public boolean tagValueEndsWith(int tag, String value) {
         String dcmValue = dcm.getString(tag).orElse(null);
-        return dcmValue != null ? dcmValue.endsWith(value) : false;
+        return dcmValue != null && dcmValue.endsWith(value);
     }
 
-    public static void expressionValidation(String condition) throws Exception {
+    public static void expressionValidation(String condition) {
         ExprConditionKheops exprConditionKheops = new ExprConditionKheops(DicomObject.newDicomObject());
         ExpressionResult.get(condition, exprConditionKheops, Boolean.class);
     }
@@ -67,49 +59,4 @@ public class ExprConditionKheops implements ExpressionItem{
         String cleanTag = tag.replaceAll("[(),]", "").toUpperCase();
         return TagUtils.intFromHexString(cleanTag);
     }
-
-    /*
-    public static boolean validateCondition(String condition) {
-        if (!condition.contains("tagValueIsPresent") && !condition.contains("tagValueContains") &&
-            !condition.contains("tagValueBeginWith") && !condition.contains("tagValueEndWith")) {
-            return false;
-        }
-
-        String hexTagInCondition = getHexTagInCondition(condition);
-        if (!hexTagInCondition.equals("")) {
-            return isHexTag(hexTagInCondition);
-        }
-
-        String tagObject = getTagObjectInCondition(condition);
-        if (!tagObject.equals("")) {
-            try {
-                new Tag().getClass().getField(tagObject.split("\\.")[1]);
-            } catch (Exception e){
-                return false;
-            }
-            return true;
-        }
-        return false;
-    }
-
-    private static String getTagObjectInCondition(String condition) {
-        Pattern pattern = Pattern.compile("(Tag\\.[a-zA-Z]+)");
-        Matcher matcher = pattern.matcher(condition);
-        if (matcher.find())
-        {
-            return matcher.group(1);
-        }
-        return "";
-    }
-
-    private static String getHexTagInCondition(String condition) {
-        Pattern pattern = Pattern.compile("((\\()[0-9a-fA-F]{4},?[0-9a-fA-F]{4}(\\)))");
-        Matcher matcher = pattern.matcher(condition);
-        if (matcher.find())
-        {
-            return matcher.group(1);
-        }
-        return "";
-    }
-     */
 }
