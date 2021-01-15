c451fb807f6fc20e954ee39f4a7d44eb8a8d5ce6
cicciu <salvatore.cicciu@gmail.com>
2021-01-15 17:41:28 +0100

    fix temporary keycloak bug by commenting keycloack code

--- src/main/java/org/karnak/StartApplication.java
+++ src/main/java/org/karnak/StartApplication.java
@@ -2,7 +2,6 @@ package org.karnak;
 
 import com.vaadin.flow.spring.annotation.EnableVaadin;
 import org.karnak.backend.config.AppConfig;
-import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,14 +10,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 @EntityScan("org.karnak.backend.data.entity")
 @EnableJpaRepositories("org.karnak.backend.data.repo")
 @EnableVaadin(value = "org.karnak")
-@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class, basePackages = "org.karnak")
+//@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class, basePackages = "org.karnak")
 public class StartApplication implements CommandLineRunner {
 
   private static final Logger log = LoggerFactory.getLogger(StartApplication.class);
