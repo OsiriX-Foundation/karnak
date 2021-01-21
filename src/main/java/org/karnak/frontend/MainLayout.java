c202b5924cb4d120279d9725bb47b11cd403d4a3
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2021-01-21 15:05:16 +0100

    Fix format header

--- src/main/java/org/karnak/frontend/MainLayout.java
+++ src/main/java/org/karnak/frontend/MainLayout.java
@@ -1,12 +1,12 @@
 /*
-* Copyright (c) 2020-2021 Karnak Team and other contributors.
-*
-* This program and the accompanying materials are made available under the terms of the Eclipse
-* Public License 2.0 which is available at http://www.eclipse.org/legal/epl-2.0, or the Apache
-* License, Version 2.0 which is available at https://www.apache.org/licenses/LICENSE-2.0.
-*
-* SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
-*/
+ * Copyright (c) 2020-2021 Karnak Team and other contributors.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse
+ * Public License 2.0 which is available at http://www.eclipse.org/legal/epl-2.0, or the Apache
+ * License, Version 2.0 which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
 package org.karnak.frontend;
 
 import com.vaadin.flow.component.AttachEvent;
