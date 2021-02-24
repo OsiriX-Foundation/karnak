026eec33faf84ae887c58e0349d1ad3ef33430fc
Nicolas van Dooren <vandooren.nicolas@gmail.com>
2021-02-24 16:46:13 +0100

    Verify if the module present in the DICOM is valid

--- src/main/java/org/karnak/backend/model/validation/ValidatorAttribute.java
+++ src/main/java/org/karnak/backend/model/validation/ValidatorAttribute.java
@@ -1,9 +1,16 @@
 package org.karnak.backend.model.validation;
 
 import java.util.List;
+import org.dcm4che6.data.DicomObject;
+import org.dcm4che6.data.Tag;
+import org.dcm4che6.data.VR;
 import org.karnak.backend.model.standard.Attribute;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ValidatorAttribute {
+  private static final Logger LOGGER = LoggerFactory.getLogger(ValidatorAttribute.class);
+
   public static boolean attributeNotExist(List<Attribute> attributes) {
     if (attributes.size() == 0) {
       return true;
@@ -15,16 +22,23 @@ public class ValidatorAttribute {
     if ((type.equals("1") || type.equals("1C")) && (tagValue == null || tagValue.length() == 0)) {
       return true;
     }
-    // can't be checked now ...
-    /*
-    if (type.equals("2") || type.equals("2C")) {
-      return false;
+
+    if ((type.equals("2") || type.equals("2C")) && tagValue == null) {
+      return true;
     }
-    */
+
     return false;
   }
 
-  public static boolean vrNotValid(String tagValue) {
+  public static boolean vrNotValid(String tagValue, int tag, VR vr) {
+    DicomObject dcmObj = DicomObject.newDicomObject();
+    try {
+      String val = "1234567890123456789012345678901234567890";
+      dcmObj.setString(Tag.RetrieveAETitle, VR.AE, val);
+      vr.type.elementOf(dcmObj, tag, vr, val);
+    } catch (UnsupportedOperationException exception) {
+      LOGGER.error("test" ,exception);
+    }
     return false;
   }
 }
--- src/main/java/org/karnak/backend/service/report/Reporting.java
+++ src/main/java/org/karnak/backend/service/report/Reporting.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 import org.dcm4che6.data.DicomElement;
 import org.dcm4che6.data.DicomObject;
 import org.dcm4che6.data.Tag;
+import org.dcm4che6.util.TagUtils;
 import org.karnak.backend.config.AppConfig;
 import org.karnak.backend.model.standard.Attribute;
 import org.karnak.backend.model.standard.Module;
@@ -34,16 +35,13 @@ public class Reporting {
     // TODO: Missing sequences
     for (Iterator<DicomElement> iterator = dcm.iterator(); iterator.hasNext(); ) {
       final DicomElement dcmEl = iterator.next();
-      final int tag = dcmEl.tag();
-      final String tagValue = dcm.getString(tag).orElse(null);
-      final String tagPath = MetadataDICOMObject.getTagPath(dcm, tag);
+      final String tagPath = MetadataDICOMObject.getTagPath(dcm, dcmEl.tag());
       List<Attribute> attributes = standardDICOM.getAttributesBySOP(sopUID, tagPath);
-      validationTagPath(sopUID, tagPath, tagValue, attributes);
+      validationTagPath(sopUID, dcm, dcmEl, attributes);
 
       modulesInDICOM.addAll(getModulesByAttributes(sopUID, attributes));
     }
     validationModules(sopUID, modulesInDICOM, dcm);
-
   }
 
   private HashSet<Module> getModulesByAttributes(String sopUID, List<Attribute> attributes) {
@@ -60,8 +58,11 @@ public class Reporting {
     return modules;
   }
 
-  public boolean validationTagPath(String sopUID, String tagPath, String tagValue, List<Attribute> attributes) {
-    String currentType = Attribute.getStrictedType(attributes);
+  public boolean validationTagPath(String sopUID, DicomObject dcm, DicomElement dcmEl, List<Attribute> attributes) {
+    final String tagValue = dcm.getString(dcmEl.tag()).orElse(null);
+    final String tagPath = MetadataDICOMObject.getTagPath(dcm, dcmEl.tag());
+    final String currentType = Attribute.getStrictedType(attributes);
+    boolean valid = true;
     if (ValidatorAttribute.attributeNotExist(attributes)) {
       LOGGER.error(
           String.format("Attribute %s doesn't exist in the SOP %s.", tagPath, sopUID)
@@ -73,10 +74,14 @@ public class Reporting {
       LOGGER.warn(
           String.format("The type %s for %s is not valid in the SOP %s", currentType, tagPath, sopUID)
       );
-      return false;
+      valid = false;
     }
+    /*
+    if (ValidatorAttribute.vrNotValid(tagValue, dcmEl.tag(), dcmEl.vr())) {
 
-    return true;
+    }
+    */
+    return valid;
   }
 
   public boolean validationModules(String sopUID, HashSet<Module> modulesInDICOM, DicomObject dcm) {
@@ -85,16 +90,43 @@ public class Reporting {
     if (missMandatoryModule.size() > 0) {
       LOGGER.error("Mandatories modules missing");
     }
-
-    for (Module module : modulesBySOP) {
-      List<Attribute> attributes = this.standardDICOM.getAttributeListByModule(module.getId());
+    for (Module module : modulesInDICOM) {
       Map<String, Attribute> mandatoryAttribute = this.standardDICOM.getModuleAttributesByType(module.getId(), "1");
-      Map<String, Attribute> mandatoryNullAttribute = this.standardDICOM.getModuleAttributesByType(module.getId(), "2");
-
-      System.out.println("test");
+      verifyAttributePresent(module.getId(), mandatoryAttribute, dcm, "1");
+      Map<String, Attribute> mandatoryEmptyAttribute = this.standardDICOM.getModuleAttributesByType(module.getId(), "2");
+      verifyAttributePresent(module.getId(), mandatoryEmptyAttribute, dcm, "2");
     }
 
     return true;
   }
 
+  private void verifyAttributePresent(String moduleName, Map<String, Attribute> mandatoryAttribute, DicomObject dcm, String type) {
+    List<String> notFromSeqAttributes = mandatoryAttribute.keySet().stream()
+        .filter(tagPath -> notFromSeq(tagPath))
+        .collect(Collectors.toList());
+    for (String tagPath : notFromSeqAttributes) {
+      if (!tagPath.contains("x") && !mandatoryAttribute.get(tagPath).getType().contains("c")) {
+        int tag = TagUtils.intFromHexString(tagPath);
+        String tagValue = dcm.getString(tag).orElse(null);
+
+        if (ValidatorAttribute.typeNotValid(type, tagValue)) {
+          LOGGER.error(
+              String.format("The attribute %s don't respect the type %s define in the module %s",
+                  tagPath, mandatoryAttribute.get(tagPath).getType(), moduleName)
+          );
+        }
+      }
+    }
+    List<String> fromSeqAttributes = mandatoryAttribute.keySet().stream()
+        .filter(tagPath -> fromSeq(tagPath))
+        .collect(Collectors.toList());
+  }
+
+  private boolean notFromSeq(String tagPath) {
+    return tagPath.split(":").length == 1;
+  }
+
+  private boolean fromSeq(String tagPath) {
+    return tagPath.split(":").length > 1;
+  }
 }
