999535493e6e17d75d364920f9496bd1ae76accc
JDCS <email@email.com>
2021-03-03 08:40:21 +0100

    feat: view project => handle proper selection/deselection + add default order column for grid gateway and project

--- src/main/java/org/karnak/frontend/project/ProjectView.java
+++ src/main/java/org/karnak/frontend/project/ProjectView.java
@@ -97,7 +97,7 @@ public class ProjectView extends HorizontalLayout implements HasUrlParameter<Str
 
   /** Init components */
   private void initComponents() {
-    initEditProject(editProject);
+    initEditProject(editProject, gridProject);
     initNewProjectForm(newProject);
     gridProject.setDataProvider(projectLogic);
     newResearchBinder = newProject.getBinder();
@@ -116,9 +116,10 @@ public class ProjectView extends HorizontalLayout implements HasUrlParameter<Str
    * Init the component edit project
    *
    * @param editProject Component to initialize
+   * @param gridProject Link to the grid for element selection
    */
-  private void initEditProject(EditProject editProject) {
-    projectLogic.addEditEventButtonUpdate(editProject);
+  private void initEditProject(EditProject editProject, GridProject gridProject) {
+    projectLogic.addEditEventButtonUpdate(editProject, gridProject);
     projectLogic.addEditEventButtonRemove(editProject);
     projectLogic.initEditProfileDropDown(editProject);
   }
@@ -134,6 +135,7 @@ public class ProjectView extends HorizontalLayout implements HasUrlParameter<Str
                 newProjectEntity.setSecret(HMAC.generateRandomKey());
                 projectLogic.createProject(newProjectEntity);
                 newProject.clear();
+                gridProject.select(newProjectEntity);
                 navigateProject(newProjectEntity);
               }
             });
