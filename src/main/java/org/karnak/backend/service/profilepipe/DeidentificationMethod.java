81aa797fbfb7700bdf18fc303e1cb0ef96245831
cicciu <salvatore.cicciu@gmail.com>
2021-03-16 14:16:02 +0100

    fix spotless

--- src/main/java/org/karnak/backend/service/profilepipe/DeidentificationMethod.java
+++ src/main/java/org/karnak/backend/service/profilepipe/DeidentificationMethod.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2021 Karnak Team and other contributors.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse
+ * Public License 2.0 which is available at http://www.eclipse.org/legal/epl-2.0, or the Apache
+ * License, Version 2.0 which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
 package org.karnak.backend.service.profilepipe;
 
 import java.util.Set;
@@ -32,7 +41,7 @@ public class DeidentificationMethod {
 
     dcm.setString(Tag.PatientIdentityRemoved, VR.CS, "YES");
 
-    //DeidentificationMethodCodeSequence
+    // DeidentificationMethodCodeSequence
     final Set<ProfileElementEntity> profileElementEntities =
         profileEntity.getProfileElementEntities();
     Sequence deidentificationMethodSequence =
