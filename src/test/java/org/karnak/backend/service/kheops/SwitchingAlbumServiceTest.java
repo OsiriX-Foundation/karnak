76b448256df1240008b0ad511e9b3aec708d6c7f
JDCS <email@email.com>
2021-03-25 09:47:21 +0100

    feat:add unit tests service

--- src/test/java/org/karnak/backend/service/kheops/SwitchingAlbumServiceTest.java
+++ src/test/java/org/karnak/backend/service/kheops/SwitchingAlbumServiceTest.java
@@ -9,8 +9,15 @@
  */
 package org.karnak.backend.service.kheops;
 
+import org.dcm4che6.data.DicomObject;
+import org.dcm4che6.data.Tag;
+import org.dcm4che6.data.VR;
+import org.dcm4che6.internal.DicomObjectImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.karnak.backend.data.entity.DestinationEntity;
+import org.karnak.backend.data.entity.KheopsAlbumsEntity;
+import org.karnak.backend.data.entity.ProjectEntity;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -29,14 +36,28 @@ class SwitchingAlbumServiceTest {
   @Test
   void should_() {
     // Init data
-    // TODO
+    DestinationEntity destinationEntity = new DestinationEntity();
+    KheopsAlbumsEntity kheopsAlbumsEntity = new KheopsAlbumsEntity();
+    DicomObject dicomObject = new DicomObjectImpl();
+    dicomObject.setString(Tag.StudyInstanceUID, VR.SH, "studyInstanceUID");
+    dicomObject.setString(Tag.SeriesInstanceUID, VR.SH, "seriesInstanceUID");
+    dicomObject.setString(Tag.SOPInstanceUID, VR.SH, "sopInstanceUID");
+    kheopsAlbumsEntity.setAuthorizationSource("authorizationSource");
+    kheopsAlbumsEntity.setAuthorizationDestination("authorizationDestination");
+    kheopsAlbumsEntity.setCondition("true");
+    destinationEntity.setDesidentification(true);
+    ProjectEntity projectEntity = new ProjectEntity();
+    byte[] tabByte = new byte[10];
+    tabByte[0] = 1;
+    projectEntity.setSecret(tabByte);
+    destinationEntity.setProjectEntity(projectEntity);
+    kheopsAlbumsEntity.setUrlAPI("http://karnak.com");
+    kheopsAlbumsEntity.setId(1L);
 
     // Call service
-
+    switchingAlbumService.apply(destinationEntity, kheopsAlbumsEntity, dicomObject);
 
     // Test results
 
   }
-
-
 }
