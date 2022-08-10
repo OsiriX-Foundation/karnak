7e36762cf190313ba6f0249bcdc51fe0c6e3a3e5
jdcs <jonathan.duchesne@hcuge.ch>
2022-08-10 10:17:47 +0200

    feat: test call garbage

--- src/main/java/org/karnak/backend/service/MemoryService.java
+++ src/main/java/org/karnak/backend/service/MemoryService.java
@@ -1,18 +1,30 @@
 package org.karnak.backend.service;
 
 import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
+
 public class MemoryService {
 
-  @Scheduled(fixedRate = 30 * 1000)
+  private static final Logger LOGGER = LoggerFactory.getLogger(MemoryService.class);
+
+
+  @Scheduled(fixedRate = 10 * 1000)
   public void callGarbageCollector() {
+    LOGGER.info("Memory:%d \t \t %d \t \t %d".formatted(Runtime.getRuntime().freeMemory(),
+        Runtime.getRuntime().totalMemory(), Runtime.getRuntime().maxMemory()));
+
     System.gc();
     System.runFinalization();
     System.gc();
     System.runFinalization();
+
+    LOGGER.info("Memory:%d \t \t %d \t \t %d".formatted(Runtime.getRuntime().freeMemory(),
+        Runtime.getRuntime().totalMemory(), Runtime.getRuntime().maxMemory()));
     try {
       TimeUnit.MILLISECONDS.sleep(200);
     } catch (InterruptedException et) {
