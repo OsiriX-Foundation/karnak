7462b7d34d32ab52e7f2a13698747f1ef01ccf68
Nicolas Roduit <nicolas.roduit@gmail.com>
2022-11-03 18:23:48 +0100

    Update libs

--- src/main/java/org/karnak/backend/service/EchoService.java
+++ src/main/java/org/karnak/backend/service/EchoService.java
@@ -77,9 +77,8 @@ public class EchoService {
     destinations.forEach(
         destination -> {
           // Case DICOM
-          if (destination instanceof DicomForwardDestination) {
-            DicomNode calledNode =
-                ((DicomForwardDestination) destination).getStreamSCU().getCalledNode();
+          if (destination instanceof DicomForwardDestination d) {
+            DicomNode calledNode = d.getStreamSCU().getCalledNode();
             // Retrieve the status of the dicom node
             DicomState dicomState =
                 Echo.process(buildEchoProcessParams(3000, 5000), sourceNode, calledNode);
@@ -88,8 +87,7 @@ public class EchoService {
                 new DestinationEcho(calledNode.getAet(), null, dicomState.getStatus()));
           }
           // Case Stow
-          else if (destination instanceof WebForwardDestination) {
-            WebForwardDestination d = (WebForwardDestination) destination;
+          else if (destination instanceof WebForwardDestination d) {
             // Add the destination and its status
             destinationEchos.add(new DestinationEcho(null, d.getRequestURL(), 0));
           }
